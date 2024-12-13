de0c47cd51b5734a60f0b7a6221edcd6a830e6f1
Delerious28 <118252809+Delerious28@users.noreply.github.com>
2024-12-13 10:43:49 +0100

    header

--- html/header.php
+++ html/header.php
@@ -4,15 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Images Coming Together</title>
-    <link href="../css/header.css" rel="stylesheet">
+    <link href="css/header.css" rel="stylesheet">
 </head>
 <body>
 
 <header>
 
 <div class="header-container">
-    <img src="../img/Chief2.png" alt="Image Left" class="image image-left">
-    <img src="../img/chief1.png" alt="Image Right" class="image image-right">
+    <img src="img/Chief2.png" alt="Image Left" class="image image-left">
+    <img src="img/chiefs4.png" alt="Image Right" class="image image-right">
     <button class="button">Uitloggen</button>
 </div>
 
