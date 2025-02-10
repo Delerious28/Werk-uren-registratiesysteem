5303f7146f2e5c7dd46e4cc0a2cd9b77b484ecfc
Christian_de_Winter <christian.bitacademy@gmail.com>
2025-02-10 13:06:19 +0100

    commi

--- admin/admin-index.php
+++ admin/admin-index.php
@@ -115,7 +115,7 @@ try {
                         <td><?= htmlspecialchars($row["Vr"]) ?></td>
                         <td><strong><?= htmlspecialchars($total) ?> Totaal</strong></td>
                     <?php else: ?>
-                        <td><?= htmlspecialchars($row["totaal"]) ?> totaal</td>
+                        <td><?= htmlspecialchars($row["totaal"]) ?> Totaal</td>
                     <?php endif; ?>
                 </tr>
             <?php endforeach; ?>
