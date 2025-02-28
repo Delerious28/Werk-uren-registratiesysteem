7ae86ce62baf8f65a390ada2818cdd241b22c983
Delerious28 <118252809+Delerious28@users.noreply.github.com>
2025-02-28 13:18:43 +0100

    Update werk_uren_registratiesysteem.sql

--- sql/werk_uren_registratiesysteem.sql
+++ sql/werk_uren_registratiesysteem.sql
@@ -3,13 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 27, 2025 at 11:27 AM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Feb 28, 2025 at 01:13 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -17,8 +18,53 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `werk_uren_registratiesysteem`
+-- Database: `werk_uren_registratiesysteem2`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chiefs`
+--
+
+CREATE TABLE `chiefs` (
+  `chief_id` int(11) NOT NULL,
+  `telefoon` varchar(50) NOT NULL,
+  `adres` varchar(255) NOT NULL,
+  `Bedrijfnaam` varchar(255) NOT NULL,
+  `stad` varchar(255) NOT NULL,
+  `postcode` varchar(10) NOT NULL,
+  `provincie` varchar(255) DEFAULT NULL,
+  `land` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `chiefs`
+--
+
+INSERT INTO `chiefs` (`chief_id`, `telefoon`, `adres`, `Bedrijfnaam`, `stad`, `postcode`, `provincie`, `land`) VALUES
+(1, '088 786 0100', '1119 Tupolevlaan 1', 'Chiefs of IT1', 'Schiphol-Rijk1', '1119 NW1', 'Noord-Holland1', 'Nederland1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact`
+--
+
+CREATE TABLE `contact` (
+  `contact_id` int(11) NOT NULL,
+  `voornaam` varchar(255) DEFAULT NULL,
+  `achternaam` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `telefoon` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`contact_id`, `voornaam`, `achternaam`, `email`, `telefoon`) VALUES
+(1, 'test', 'Gebruiker', 'test@example.com1', '06123456781');
 
 -- --------------------------------------------------------
 
@@ -27,15 +73,55 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `hours` (
-                         `hours_id` int(11) NOT NULL AUTO_INCREMENT,
-                         `user_id` int(11) NOT NULL,
-                         `date` date NOT NULL,
-                         `hours` tinyint(2) NOT NULL CHECK (`hours` BETWEEN 0 AND 24),
-                         `accord` enum('Pending','Approved','Rejected') NOT NULL DEFAULT 'Pending',
-                         PRIMARY KEY (`hours_id`),
-                         KEY `user_id` (`user_id`),
-                         UNIQUE KEY `unique_user_date` (`user_id`,`date`)
+  `hours_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `start_hours` datetime NOT NULL,
+  `eind_hours` datetime NOT NULL,
+  `hours` tinyint(2) NOT NULL CHECK (`hours` between 0 and 24),
+  `accord` enum('Pending','Approved','Rejected') NOT NULL DEFAULT 'Pending',
+  `contract_hours` tinyint(2) NOT NULL CHECK (`contract_hours` between 0 and 24)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `klant`
+--
+
+CREATE TABLE `klant` (
+  `klant_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  `voornaam` varchar(255) NOT NULL,
+  `achternaam` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefoon` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `klant`
+--
+
+INSERT INTO `klant` (`klant_id`, `project_id`, `voornaam`, `achternaam`, `email`, `telefoon`) VALUES
+(1, 1, 'John1', 'Doe1', 'johndoe@example.com1', '06123456781');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `project_id` int(11) NOT NULL,
+  `project_naam` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`project_id`, `project_naam`) VALUES
+(1, 'Project 1');
 
 -- --------------------------------------------------------
 
@@ -44,29 +130,112 @@ CREATE TABLE `hours` (
 --
 
 CREATE TABLE `users` (
-                         `user_id` int(11) NOT NULL AUTO_INCREMENT,
-                         `name` varchar(255) NOT NULL,
-                         `password` varchar(255) NOT NULL,
-                         `role` varchar(50) NOT NULL,
-                         PRIMARY KEY (`user_id`)
+  `user_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `achternaam` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefoon` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`name`, `password`, `role`, `user_id`) VALUES
-                                                                ('yehia', '$2y$10$8VG79818awlStVRxWu4O5.zaLdb2zwAkNjUYEXraQlwjLC.SiL6/i', 'user', 8),
-                                                                ('Beau', '$2y$10$hO4jjhDNRNG1jMfM3nTkc.3cawInIMNMcWSW9agbvbvWROeO5Lf7C', 'user', 9),
-                                                                ('admin', '$2y$10$pE4NJDSy6F4C1VhbgKGelejTigSsqBrSpKht8AlBwq7sxQz0XAGQC', 'admin', 10);
+INSERT INTO `users` (`user_id`, `name`, `achternaam`, `email`, `telefoon`, `password`, `role`) VALUES
+(1, 'Beau', 'Sulzle', 'beau@example.com', '0612345678', '$2y$10$hO4jjhDNRNG1jMfM3nTkc.3cawInIMNMcWSW9agbvbvWROeO5Lf7C', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chiefs`
+--
+ALTER TABLE `chiefs`
+  ADD PRIMARY KEY (`chief_id`);
+
+--
+-- Indexes for table `contact`
+--
+ALTER TABLE `contact`
+  ADD PRIMARY KEY (`contact_id`);
+
+--
+-- Indexes for table `hours`
+--
+ALTER TABLE `hours`
+  ADD PRIMARY KEY (`hours_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `klant`
+--
+ALTER TABLE `klant`
+  ADD PRIMARY KEY (`klant_id`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`project_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chiefs`
+--
+ALTER TABLE `chiefs`
+  MODIFY `chief_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `contact`
+--
+ALTER TABLE `contact`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `hours`
+--
+ALTER TABLE `hours`
+  MODIFY `hours_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `klant`
+--
+ALTER TABLE `klant`
+  MODIFY `klant_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `project_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
 --
 
+--
+-- Constraints for table `hours`
+--
 ALTER TABLE `hours`
-    ADD CONSTRAINT `hours_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
-
+  ADD CONSTRAINT `hours_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
