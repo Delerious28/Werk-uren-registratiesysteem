9aabef92d36a1707eec7e2cb1d7729f2ebf749ac
Christian_de_Winter <christian.bitacademy@gmail.com>
2025-02-28 11:37:59 +0100

    merge

--- uren-registreren.php
+++ uren-registreren.php
@@ -2,7 +2,7 @@
 require 'db/conn.php';
 
 // Klanten ophalen
-$klantenQuery = "SELECT DISTINCT klant_id, klant_voornaam, klant_achternaam FROM klant";
+$klantenQuery = "SELECT klant_id, klant_voornaam, klant_achternaam FROM klant";
 $klantenStmt = $pdo->prepare($klantenQuery);
 $klantenStmt->execute();
 $klanten = $klantenStmt->fetchAll(PDO::FETCH_ASSOC);
@@ -13,12 +13,27 @@ $selectedKlant = isset($_POST['klant']) ? $_POST['klant'] : "";
 // Projecten ophalen op basis van de geselecteerde klant
 $projecten = [];
 if (!empty($selectedKlant)) {
-    $projectenQuery = "SELECT p.project_naam FROM project p 
-                       JOIN klant k ON p.project_id = k.project_id 
+    $projectenQuery = "SELECT p.project_id, p.project_naam 
+                       FROM project p
+                       JOIN klant k ON k.project_id = p.project_id
                        WHERE k.klant_id = ?";
     $projectenStmt = $pdo->prepare($projectenQuery);
     $projectenStmt->execute([$selectedKlant]);
-    $projecten = $projectenStmt->fetchAll(PDO::FETCH_COLUMN);
+    $projecten = $projectenStmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+$totaalUren = "";
+if (isset($_POST['begin']) && isset($_POST['eind'])) {
+    $startTijd = strtotime($_POST['begin']);
+    $eindTijd = strtotime($_POST['eind']);
+
+    // Zorg dat de eindtijd later is dan de starttijd
+    if ($eindTijd > $startTijd) {
+        $urenVerschil = ($eindTijd - $startTijd) / 3600; // Converteer seconden naar uren
+        $totaalUren = $urenVerschil . " uur";
+    } else {
+        $totaalUren = "Ongeldige tijd";
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -47,51 +62,53 @@ if (!empty($selectedKlant)) {
         <form method="POST">
             <div class="blok-1">
                 <label>Klant:</label>
-                <input list="klantenn" name="klant" class="small-input" value="<?= htmlspecialchars($selectedKlant); ?>" onchange="this.form.submit()">
-                <datalist id="klantenn">
+                <select name="klant" class="small-input" onchange="this.form.submit()">
+                    <option value="">-- Kies Klant --</option>
                     <?php foreach ($klanten as $klant): ?>
-                        <option value="<?= $klant['klant_id']; ?>"><?= htmlspecialchars($klant['klant_voornaam'] . " " . $klant['klant_achternaam']); ?></option>
+                        <option value="<?= $klant['klant_id']; ?>" <?= ($klant['klant_id'] == $selectedKlant) ? 'selected' : ''; ?>>
+                            <?= $klant['klant_voornaam'] . ' ' . $klant['klant_achternaam']; ?>
+                        </option>
                     <?php endforeach; ?>
-                </datalist>
+                </select>
 
                 <label>Project naam:</label>
-                <input list="projs" name="project" class="small-input">
-                <datalist id="projs">
+                <select name="project" class="small-input">
+                    <option value="">-- Kies Project --</option>
                     <?php foreach ($projecten as $project): ?>
-                        <option value="<?= htmlspecialchars($project); ?>"></option>
+                        <option value="<?= $project['project_id']; ?>" <?= (isset($_POST['project']) && $_POST['project'] == $project['project_id']) ? 'selected' : ''; ?>>
+                            <?= $project['project_naam']; ?>
+                        </option>
                     <?php endforeach; ?>
-                </datalist>
+                </select>
 
                 <label>Beschrijving:</label>
                 <input type="text" name="beschrijving" class="small-input">
 
                 <label>Starttijd:</label>
-                <select name="begin">
-                    <option>08:00</option>
-                    <option>09:00</option>
-                    <option>10:00</option>
-                    <option>11:00</option>
-                    <option>12:00</option>
-                    <option>13:00</option>
-                    <option>14:00</option>
-                    <option>15:00</option>
-                    <option>16:00</option>
+                <select name="begin" onchange="this.form.submit()">
+                    <option value="">-- Kies --</option>
+                    <option value="08:00" <?= (isset($_POST['begin']) && $_POST['begin'] == '08:00') ? 'selected' : ''; ?>>08:00</option>
+                    <option value="09:00" <?= (isset($_POST['begin']) && $_POST['begin'] == '09:00') ? 'selected' : ''; ?>>09:00</option>
+                    <option value="10:00" <?= (isset($_POST['begin']) && $_POST['begin'] == '10:00') ? 'selected' : ''; ?>>10:00</option>
+                    <option value="11:00" <?= (isset($_POST['begin']) && $_POST['begin'] == '11:00') ? 'selected' : ''; ?>>11:00</option>
+                    <option value="12:00" <?= (isset($_POST['begin']) && $_POST['begin'] == '12:00') ? 'selected' : ''; ?>>12:00</option>
                 </select>
 
                 <label>Eindtijd:</label>
-                <select name="eind">
-                    <option>12:00</option>
-                    <option>13:00</option>
-                    <option>14:00</option>
-                    <option>15:00</option>
-                    <option>16:00</option>
-                    <option>17:00</option>
-                    <option>18:00</option>
-                    <option>19:00</option>
+                <select name="eind" onchange="this.form.submit()">
+                    <option value="">-- Kies --</option>
+                    <option value="12:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '12:00') ? 'selected' : ''; ?>>12:00</option>
+                    <option value="13:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '13:00') ? 'selected' : ''; ?>>13:00</option>
+                    <option value="14:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '14:00') ? 'selected' : ''; ?>>14:00</option>
+                    <option value="15:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '15:00') ? 'selected' : ''; ?>>15:00</option>
+                    <option value="16:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '16:00') ? 'selected' : ''; ?>>16:00</option>
+                    <option value="17:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '17:00') ? 'selected' : ''; ?>>17:00</option>
+                    <option value="18:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '18:00') ? 'selected' : ''; ?>>18:00</option>
+                    <option value="19:00" <?= (isset($_POST['eind']) && $_POST['eind'] == '19:00') ? 'selected' : ''; ?>>19:00</option>
                 </select>
 
                 <label>Uren totaal:</label>
-                <input type="text" id="totaaluren" readonly class="small-input">
+                <input type="text" id="totaaluren" name="totaaluren" value="<?= htmlspecialchars($totaalUren); ?>" readonly class="small-input">
 
                 <button type="submit">+ Voeg toe</button>
             </div>
